5be924c69251adf94692a26ad99f820b93e7c408
Aboodkhaled-aa2 <abbba4285@gmail.com>
2026-09-15 16:30:39 +0300

    Replace getServerAuthSession with getServerSession

--- app/api/stripe/checkout/route.ts
+++ app/api/stripe/checkout/route.ts
@@ -1,11 +1,12 @@
 import { NextResponse } from "next/server";
-import { getServerAuthSession } from "@/server/auth";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { stripe } from "@/lib/stripe";
 import { env } from "@/env.mjs";
 
 export async function POST(req: Request) {
   try {
-    const session = await getServerAuthSession();
+    const session = await getServerSession(authOptions);
 
     if (!session || !session.user) {
       return new NextResponse("Unauthorized", { status: 401 });
