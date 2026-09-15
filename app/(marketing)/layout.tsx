84b708cdff7ce33ed7249621322fb39d1d023962
Aboodkhaled-aa2 <abbba4285@gmail.com>
2026-09-15 11:49:44 +0300

    Update layout.tsx

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,18 +1,13 @@
-import { NavBar } from "@/components/layout/navbar";
-import { SiteFooter } from "@/components/layout/site-footer";
-import { NavMobile } from "@/components/layout/mobile-nav";
+import React from 'react';
 
 interface MarketingLayoutProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 export default function MarketingLayout({ children }: MarketingLayoutProps) {
-  return (
-    <div className="flex min-h-screen flex-col">
-      <NavMobile />
-      <NavBar scroll={true} />
-      <main className="flex-1">{children}</main>
-      <SiteFooter />
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-[#0b0f19] text-gray-100 flex flex-col">
+            {children}
+        </div>
+    );
 }
