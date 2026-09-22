fb64abcc68493a8ec861cae1d61957dcad3c82c7
Aboodkhaled-aa2 <abbba4285@gmail.com>
2026-09-23 00:15:04 +0300

    Refactor section-columns component layout and styles
    Updated styling and layout for section columns in the dashboard.

--- components/dashboard/section-columns.tsx
+++ components/dashboard/section-columns.tsx
@@ -12,13 +12,19 @@ export function SectionColumns({
   children,
 }: SectionColumnsType) {
   return (
-    <div className="grid grid-cols-1 gap-x-10 gap-y-4 py-8 md:grid-cols-10">
-      <div className="col-span-4 space-y-1.5">
-        <h2 className="text-lg font-semibold leading-none">{title}</h2>
-        <p className="text-balance text-sm text-muted-foreground">
-          {description}
-        </p>
+    <div className="grid grid-cols-1 gap-x-10 gap-y-6 border-b border-slate-800/80 py-8 md:grid-cols-10">
+      <div className="col-span-4 space-y-2">
+        <h2 className="text-lg font-semibold leading-none text-white">
+          {title}
+        </h2>
+
+        {description && (
+          <p className="max-w-md text-balance text-sm leading-6 text-slate-500">
+            {description}
+          </p>
+        )}
       </div>
+
       <div className="col-span-6">{children}</div>
     </div>
   );
