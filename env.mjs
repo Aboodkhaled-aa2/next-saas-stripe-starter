fadcb8a064fea7e3d25546bb16b5b013354aec5a
Aboodkhaled-aa2 <abbba4285@gmail.com>
2026-09-25 00:12:50 +0300

    Fix production env validation

--- env.mjs
+++ env.mjs
@@ -27,14 +27,14 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_APP_URL: z.string().optional(),
 
-    NEXT_PUBLIC_STRIPE_STARTER_MONTHLY_PLAN_ID: z.string().min(1),
-    NEXT_PUBLIC_STRIPE_STARTER_YEARLY_PLAN_ID: z.string().min(1),
+    NEXT_PUBLIC_STRIPE_STARTER_MONTHLY_PLAN_ID: z.string().optional(),
+    NEXT_PUBLIC_STRIPE_STARTER_YEARLY_PLAN_ID: z.string().optional(),
 
-    NEXT_PUBLIC_STRIPE_PRO_MONTHLY_PLAN_ID: z.string().min(1),
-    NEXT_PUBLIC_STRIPE_PRO_YEARLY_PLAN_ID: z.string().min(1),
+    NEXT_PUBLIC_STRIPE_PRO_MONTHLY_PLAN_ID: z.string().optional(),
+    NEXT_PUBLIC_STRIPE_PRO_YEARLY_PLAN_ID: z.string().optional(),
 
-    NEXT_PUBLIC_STRIPE_BUSINESS_MONTHLY_PLAN_ID: z.string().min(1),
-    NEXT_PUBLIC_STRIPE_BUSINESS_YEARLY_PLAN_ID: z.string().min(1),
+    NEXT_PUBLIC_STRIPE_BUSINESS_MONTHLY_PLAN_ID: z.string().optional(),
+    NEXT_PUBLIC_STRIPE_BUSINESS_YEARLY_PLAN_ID: z.string().optional(),
   },
 
   runtimeEnv: {
